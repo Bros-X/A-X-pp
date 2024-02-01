256c1a3166862fd898fdff42fd6197926dea2163
Bros-X <153162912+Bros-X@users.noreply.github.com>
2024-02-01 11:41:32 +0530

    eXeiT3.js

--- eXT/eXeiT3.js
+++ eXT/eXeiT3.js
@@ -1,7 +1,7 @@
 let DatAa = `<style>
 .gone{position:fixed;left:0;top:0;bottom:0;right:0
 ;z-index:9999999999999999;background:rgba(0,0,0,0.5);
-display:grid;place-items:center;display:none;
+display:grid;place-items:center;
 }
 
 </style>
