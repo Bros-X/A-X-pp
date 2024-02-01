cea222c0edd76912318d7773b8c044df3ccd51b9
Bros-X <153162912+Bros-X@users.noreply.github.com>
2024-02-01 12:35:52 +0530

    eXeiT5.js

--- eXT/eXeiT5.js
+++ eXT/eXeiT5.js
@@ -1,6 +1,6 @@
 let DatAa = `<style>
 .gone{position:fixed;left:0;top:0;bottom:0;right:0
-;z-index:9999999999999999;background:rgba(0,0,0,0.5);
+;z-index:9;background:rgba(0,0,0,0.5);
 display:grid;place-items:center;
 }
 
