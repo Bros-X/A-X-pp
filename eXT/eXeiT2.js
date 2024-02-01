6ea074ad7b5f3838e1a52a7dbd50c9a260f94415
Bros-X <153162912+Bros-X@users.noreply.github.com>
2024-02-01 11:27:50 +0530

    eXeiT2.js

--- eXT/eXeiT2.js
+++ eXT/eXeiT2.js
@@ -1,11 +1,24 @@
 let DatAa = `<style>
 .gone{position:fixed;left:0;top:0;bottom:0;right:0
 ;z-index:9999999999999999;background:rgba(0,0,0,0.5);
-display:grid;place-items:center;
+display:grid;place-items:center;display:none;
 }
 
 </style>
-<div class="gone">Hii</div>
+<div class="gone" id="sHwC">Hiibsn 
+<button onclick="hDwZ()"></button>
+</div>
+<script>
+let sCW = document.getElementById("sHwC");
+function sHwZ(){
+sCW.style.display = "grid";
+}
+
+function hDwZ(){
+sCW.style.display = "none";
+}
+
+</script>
 
 `;
 Dta = DatAa;
