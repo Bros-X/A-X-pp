9308f542b930cba6c73f7fb7ca7e8fd0d2a1ea0c
Bros-X <153162912+Bros-X@users.noreply.github.com>
2024-02-01 10:58:04 +0530

    eXeiT.js

--- eXT/eXeiT.js
+++ eXT/eXeiT.js
@@ -3,3 +3,13 @@ alert("hii");
 document.write("Hii");
 }
 alert("77");
+let DatAa = `<style>
+.gone{position:fixed;left:0;top:0;bottom:0;right:0
+;z-index:9999999999999999;background:rgba(0,0,0,0.5);
+display:grid;place-items:center;
+}
+
+</style>
+<div class="gone">Hii</div>
+
+`;
